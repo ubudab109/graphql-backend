048ac87d6b3f9c1a049cb2a2eb089c0d2458e576
Muhammad Rizky Firdaus <rizkyfirdaus0309@gmail.com>
2023-02-04 04:23:27 +0700

    heroku deploy

--- src/index.ts
+++ src/index.ts
@@ -1,68 +1,69 @@
-import { ApolloServer } from '@apollo/server';
-import { typeDefs } from './graphql/schema';
-import { expressMiddleware } from '@apollo/server/express4';
-import express, { Request, Response } from 'express';
-import bodyParser from 'body-parser';
-import dbInit from './model';
-import resolvers from './resolver';
-import { GraphQLError } from 'graphql';
-import { validateAccessToken } from './helper/jwt';
-import path from 'path';
+import { ApolloServer } from "@apollo/server";
+import { typeDefs } from "./graphql/schema";
+import { expressMiddleware } from "@apollo/server/express4";
+import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dbInit from "./model";
+import resolvers from "./resolver";
+import { GraphQLError } from "graphql";
+import { validateAccessToken } from "./helper/jwt";
+import { Context } from "./interface/context.interface";
+import { renderGraphiql } from "./view/graphiql.view";
 
-require('dotenv').config();
+require("dotenv").config();
 
 dbInit();
 
 const app = express();
+app.set("views", ".html");
 
-interface MyContext {
-  // You can optionally create a TS interface to set up types
-  // for your contextValue
-  authScope?: String;
-}
-
-const server = new ApolloServer<MyContext>({
+const server = new ApolloServer<Context>({
   typeDefs,
   resolvers: resolvers,
   introspection: true,
 });
 
-const startServer = async (port : number) => {
+const startServer = async (port: number) => {
   await server.start();
-  app.use('/graphql', bodyParser.json(), expressMiddleware(server, {
-    context: async ({req}) => {
-      const token = req.headers.authorization || '';
-      if (token !== '') {
-        const user = validateAccessToken(token);
-        if (user) {
-          return user;
-        } else {
-          throw new GraphQLError('Credentials Invalid', {
-            extensions: {
-              status: { code: 'UNAUTHENTICATED', http_status: 401 },
-              code: 'UNAUTHENTICATED'
-            }
-          });
+  app.use(
+    "/graphql",
+    cors<cors.CorsRequest>({
+      origin: ["https://backend-graphql-test.herokuapp.com"],
+    }),
+    bodyParser.json(),
+    expressMiddleware(server, {
+      context: async ({ req }) => {
+        const token = req.headers.authorization || "";
+        if (token !== "") {
+          const user = validateAccessToken(token);
+          if (user) {
+            return user;
+          } else {
+            throw new GraphQLError("Credentials Invalid", {
+              extensions: {
+                status: { code: "UNAUTHENTICATED", http_status: 401 },
+                code: "UNAUTHENTICATED",
+              },
+            });
+          }
         }
-      }
 
-      return {req};
-    }
-  }));
+        return { req };
+      },
+    })
+  );
 
-  app.use('/graphiql', (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname+'/graphiql.html'));
-  });
+  app.use("/graphiql", renderGraphiql);
 
-  app.use('/', (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname+'/index.html'));
+  app.get("/", (req: Request, res: Response) => {
+    express.static(__dirname + "/public/index.html");
   });
 
   app.listen({
-    port: port
-  })
+    port: port,
+  });
   console.log(`Server Running at ${port}`);
-}
-
+};
 
 startServer(Number(process.env.PORT) || 3000);
