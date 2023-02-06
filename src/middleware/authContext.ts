8e8dd07fe7dd7b83ceb7567e8b0663b068e5f932
Muhammad Rizky Firdaus <rizkyfirdaus0309@gmail.com>
2023-02-06 09:36:14 +0700

    Update authContext.ts

--- src/middleware/authContext.ts
+++ src/middleware/authContext.ts
@@ -1,5 +1,10 @@
 import { GraphQLError } from "graphql"
 
+/**
+ * If the user is not logged in, throw an error. Otherwise, return true.
+ * @param {any} data - any - This is the data that is passed to the resolver.
+ * @returns a boolean value.
+ */
 export const isLoggedIn = (data: any) => {
   if (!data.user) {
     throw new GraphQLError('JSON Token Invalid', {
